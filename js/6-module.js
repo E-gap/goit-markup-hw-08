0e95a5918686ee3d902c8db36a0c33c742f5fff6
E-gap <106596181+E-gap@users.noreply.github.com>
2022-10-04 11:20:40 +0300

    04.10.2022

--- js/6-module.js
+++ js/6-module.js
@@ -259,7 +259,7 @@ function handleSubmit(event) {
   event.currentTarget.reset();
 } */
 
-const button = document.querySelector(".btn");
+/* const button = document.querySelector(".btn");
 
 const handleClick = (event) => {
   console.log("event: ", event);
@@ -267,4 +267,22 @@ const handleClick = (event) => {
   console.log("currentTarget: ", event.currentTarget);
 };
 
-button.addEventListener("click", handleClick);
+button.addEventListener("click", handleClick); */
+
+//const listWithId = document.querySelector('#menu');
+//listWithId.style.textTransform = 'uppercase';
+//listWithId.style.fontSize = '24px';
+//console.log(listWithId);
+
+//const listWithClass = document.querySelector('.menu');
+//console.log(listWithClass);
+
+//const menuItemsByTagName = document.querySelectorAll("li");
+//console.log(menuItemsByTagName);
+
+//const menuItemsByClass = document.querySelectorAll(".menu-item");
+//console.log(menuItemsByClass);
+
+//const firstMenuItem = document.querySelector(".menu-item");
+//firstMenuItem.style.color = 'tomato';
+//console.log(firstMenuItem);
