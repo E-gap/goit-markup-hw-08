cca9b31c8a14fe87dacff67384b30e82a2ab54a9
E-gap <106596181+E-gap@users.noreply.github.com>
2022-10-11 15:09:05 +0300

    11.10.2022

--- js/7-module.js
+++ js/7-module.js
@@ -87,3 +87,48 @@ function getRandomHex() {
         .toString(16)
         .padStart(2, "0");
 } */
+
+/* const colorPalette = document.querySelector(".color-palette");
+const output = document.querySelector(".output");
+
+
+createPaletteItems();
+
+function createPaletteItems() {
+  const items = [];
+  for (let i = 0; i < 60; i++) {
+    const color = getRangomColor();
+    const item = document.createElement("button");
+    item.type = "button";
+    item.dataset.color = color;
+    item.style.backgroundColor = color;
+    item.classList.add("item");
+    items.push(item);
+  }
+  colorPalette.append(...items);
+}
+
+function getRangomColor() {
+  return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
+}
+
+function getRandomHex() {
+  return Math.round(Math.random() * 256)
+    .toString(16)
+    .padStart(2, "0");
+}
+
+colorPalette.addEventListener("click", selectColor);
+
+function selectColor(event) {
+  if (event.target.nodeName !== "BUTTON") {
+    return;
+  }
+
+  const selectedColor = event.target.dataset.color;
+  output.textContent = `Selected color: ${selectedColor}`;
+  output.style.color = selectedColor;
+} */
+
+const result = _.add(2, 3);
+console.log(result); // 5
